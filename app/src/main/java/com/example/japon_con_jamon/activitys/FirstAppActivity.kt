30b168875ea71b63f97571a151c25e0060d63485
JaimeRico <jimmyrrich@gmail.com>
2024-08-21 13:25:55 +0200

    Primer Testeo del text-speech y estructura basica del json

---
+++ app/src/main/java/com/example/japon_con_jamon/activitys/FirstAppActivity.kt
@@ -0,0 +1,61 @@
+package com.example.japon_con_jamon.activitys
+
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.japon_con_jamon.R
+import java.util.Locale
+
+class FirstAppActivity : AppCompatActivity(), TextToSpeech.OnInitListener{
+    private lateinit var textToSpeech: TextToSpeech
+    private lateinit var button: Button
+    private lateinit var textView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Instanciar TextToSpeech
+        textToSpeech = TextToSpeech(this,this)
+
+        // Obtener referencias a las vistas
+        button = findViewById(R.id.boton1)
+        textView = findViewById(R.id.texto1)
+
+        // Configurar el click listener del botón
+        button.setOnClickListener {
+            // Leer el texto del TextView con TextToSpeech
+            val textToSpeak = textView.text.toString()
+            textToSpeech.speak(textToSpeak, TextToSpeech.QUEUE_FLUSH, null, null)
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            // Configurar el idioma para el TextToSpeech, por ejemplo Español (España)
+            val result = textToSpeech.setLanguage(Locale("es", "ES"))
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                // Manejar el caso donde el idioma no es soportado
+                println("El idioma no está soportado")
+            }
+        } else {
+            // Manejar el error de inicialización
+            println("Inicialización de TextToSpeech fallida")
+        }
+    }
+
+    override fun onDestroy() {
+        // Liberar recursos de TextToSpeech cuando ya no se necesite
+        if (::textToSpeech.isInitialized) {
+            textToSpeech.stop()
+            textToSpeech.shutdown()
+        }
+        super.onDestroy()
+    }
+}
